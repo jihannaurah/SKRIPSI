d61d38299caefb308f7aea6a5fd7db8030c1ea60
jihannaurah <123360759+jihannaurah@users.noreply.github.com>
2026-05-11 14:01:32 +0700

    Create app.py for Streamlit diet recommendation app

---
+++ app.py
@@ -0,0 +1,8 @@
+%%writefile app.py
+import streamlit as st
+
+st.set_page_config(page_title="Sistem Rekomendasi Diet", layout="wide")
+st.title("🥗 Uji Coba Streamlit Jihan")
+st.write("Wah! Ternyata bikin website di Google Colab itu bisa dan gampang banget!")
+
+st.success("Tampilan kerangka dasar berhasil dimuat!")
